b57e76df39662b3bcb141057e64b194b01e83406
oleksandr.bodnarchuk@outlook.com <oleksandrbodnarchuk@presspublica.pl>
2023-05-07 21:23:18 +0200

    [Customer] - added LoadBalancer to my RestTemplate

--- microservices/customer/src/main/java/pl/alex/customer/CustomerService.java
+++ microservices/customer/src/main/java/pl/alex/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package pl.alex.customer;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -7,7 +8,6 @@ import java.util.Objects;
 
 @Service
 public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
-
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
                 .name(customerRequest.name())
@@ -19,7 +19,7 @@ public record CustomerService(CustomerRepository customerRepository, RestTemplat
         // TODO: check if email is valid
         // TODO: check if email is not taken
         // TODO: check if customer is fraudster
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
 
         if(Objects.isNull(fraudCheckResponse)|| fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("Fraudster");
