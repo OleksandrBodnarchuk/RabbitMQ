f31885a1379d90e83de5589aadb21ee5e8377db8
oleksandr.bodnarchuk@outlook.com <oleksandrbodnarchuk@presspublica.pl>
2023-05-07 19:38:03 +0200

    [Test class] - added

---
+++ rabbitMQ_01/src/test/java/com/example/rabbitmq_01/RabbitMq01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMq01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
