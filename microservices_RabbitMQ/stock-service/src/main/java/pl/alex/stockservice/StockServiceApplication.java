192aaa57e7565e3caf32d96563f6f345504e815c
oleksandr.bodnarchuk@outlook.com <oleksandrbodnarchuk@presspublica.pl>
2023-05-10 11:53:11 +0200

    [Stock] - created stock service

---
+++ microservices_RabbitMQ/stock-service/src/main/java/pl/alex/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.alex.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockServiceApplication.class, args);
+    }
+
+}
